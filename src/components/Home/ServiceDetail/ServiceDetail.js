16e2718306971e27ffd8b8f8c1c1bdb5c82771f8
Sahinur Islam <infosahinur@gmail.com>
2021-08-16 00:59:52 +0600

    service added option done

--- src/components/Home/ServiceDetail/ServiceDetail.js
+++ src/components/Home/ServiceDetail/ServiceDetail.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const ServiceDetail = ({ service }) => {
-    const { title, image, description, price } = service;
+    const { serviceTitle, image, description, price } = service;
     return (
         <div className="col-md-6 col-lg-4 service-single d-flex align-items-stretch">
             <div className="icon-box">
               <div className="icon"><img src={image} className="img-fluid" alt=""/></div>
               <br />
-              <h4 className="title">{title}</h4>
+              <h4 className="title">{serviceTitle}</h4>
               <h5>${price}</h5>
               <p className="description">{description}</p>
             </div>
