b6ff8f688678a3aab0a55649a910b5ce55effe94
Sahinur Islam <infosahinur@gmail.com>
2021-08-18 19:16:54 +0600

    redmi done

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Getting Started with Create React App
+
+**Cogency** is a full-stack single-page Agency web app.
+
+- Login system with firebase, private routes and dashboard for admins and users .
+- Users can order services, monitor their orderlist and give a review, delete or update the review.
+- Admin can add services, update and delete services.
+- Admin can add new admins and can change the status of orderlist.
+
+
+### Front-end Technology : 
+- **React, React-Router-Dom,**
+- **React-Bootstrap**
+- **CSS3**
+- **Firebase Authentication**
+
+
+### Back-end Technology : 
+- **Node**
+- **Express**
+- **MongoDB**
+
+### `Website Link :` [https://cogency-bd.web.app](https://cogency-bd.web.app)
+
+<hr/>
+
+**[Back-end Repository](https://github.com/devSahinur/Cogency-server)**
+<hr/>
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,20 +4,26 @@ import toast from 'react-hot-toast';
 import { useHistory, useParams } from 'react-router-dom';
 
 
-import { UserContext } from '../App';
 import AddReview from '../components/Dashborad/AddReview/AddReview';
 import AddService from '../components/Dashborad/AddService/AddService';
 import DashboardNavbar from '../components/Dashborad/DashboardNavbar/DashboardNavbar';
 import ManageService from '../components/Dashborad/ManageService/ManageService';
 import Profile from '../components/Dashborad/Profile/Profile';
 import Sidebar from '../components/Dashborad/Sidebar/Sidebar';
+import DashboardLoader from '../components/Dashborad/DashboardLoader'
+import MakeAdmin from '../components/Dashborad/MakeAdmin/MakeAdmin';
+import { UserContext } from '../App';
+
 
 const Dashboard = ({ adminLoading }) => {
-    const { loggedInUser: { email },  } = useContext(UserContext);
+    const { loggedInUser: { email }, isAdmin } = useContext(UserContext);
     const history = useHistory();
     const { panel } = useParams();
     const [showSidebar, setShowSidebar] = useState(false);
-    const isAdmin = false;
+    const [loadingReview, setLoadingReview] = useState(true);
+    const [review, setReview] = useState({});
+    const [reviewEdit, setReviewEdit] = useState(false);
+    // const isAdmin = false;
 
     if(
         !adminLoading &&  isAdmin && (
@@ -37,6 +43,14 @@ const Dashboard = ({ adminLoading }) => {
     ) {
         history.replace({ pathname: "/dashboard/profile" });
     }
+    useEffect(() => {
+        axios.get(`http://localhost:5000/user/review?email=${email}`)
+            .then(res => {
+                setReview(res.data);
+                setLoadingReview(false);
+            })
+            .catch(error => toast.error(error.message))
+    }, [email, reviewEdit, review])
 
 
     return (
@@ -45,8 +59,10 @@ const Dashboard = ({ adminLoading }) => {
             <div id="content">
                 <DashboardNavbar setShowSidebar={setShowSidebar} show={showSidebar} />
                 {
-                    panel === 'profile' ? <Profile/>
-                    : panel === 'addService' ? <AddService/>
+                    adminLoading ? <DashboardLoader />
+                    : panel === "profile" ? <Profile />
+                    : panel === "addService" && isAdmin ? <AddService />
+                    : panel === "makeAdmin" && isAdmin ? <MakeAdmin />
                     : panel === 'manageServices' ? <ManageService/>
                     : panel === 'review' ? <AddReview/>
                     : null
